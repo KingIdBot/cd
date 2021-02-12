240e6f9dc8c89afb53c210ff20b500da1efbe193
KingIdBot <77149298+KingIdBot@users.noreply.github.com>
2021-02-12 18:12:57 +0800

    jangan di edit gan

---
+++ database/menu/help.js
@@ -0,0 +1,209 @@
+const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+        return `
+ *〈 ${botName} 〉*
+ 
+╔══════[ *USER* ]══════
+║  ╔⌦ *Name* : ${pushname}
+║  ╠⌦ *XP* : ${reqXp}
+║  ╠⌦ *Money* : ${uangku}
+║  ╠⌦ *Registered : ✔️
+║  ╠⌦ *Prefix* : 「  ${prefix}  」
+║  ╚⌦ *Version* : 0.0.5
+╚══════════════════
+▬▭▬▭▬❰ *BOT* ❱▭▬▭▬▭
+PILIH FITUR DI BAWAH INI YAA
+▬▭▬▭▬❰ *NOTE* ❱▭▬▭▬▭
+║ _TIDAK SEMUA FITUR WORK GAN_
+║ _SUBSCRIBE CHANNEL MRKING
+║ _BOT FULL FITUR_
+▭▬▭▬[ *UTAMA* ]▭▬▭▬▭
+
+*〈 ABOUT 〉*
+*${prefix}info* (error)
+*${prefix}blocklist*
+*${prefix}chatlist* (error)
+*${prefix}ping*
+*${prefix}bugreport* <text>
+
+*〈 STICKER MAKER 〉*
+
+*${prefix}sticker*
+*${prefix}stickergif*
+*${prefix}ttp*
+
+ *〈 18+ 〉*
+*${prefix}randomhentai*
+*${prefix}nsfwtrap*
+*${prefix}nsfwneko*
+
+*〈 DOWNLOADER 〉*
+*${prefix}pinterest* <query>
+*${prefix}ytmp3* <linkyt>
+*${prefix}ytmp4* <linkyt>
+*${prefix}tiktok* <linktiktok>
+   
+*〈 EDUCATION 〉*
+*${prefix}wiki* <query>
+*${prefix}wikien* <query>
+*${prefix}nulis* <text>
+*${prefix}quotes*
+*${prefix}quotes2*
+*${prefix}tafsirmimpi* <dream>
+*${prefix}translate* <language_code>
+*${prefix}artinama* <name>
+
+*〈 FUN 〉*
+*${prefix}alay* <text>
+*${prefix}bucin*
+*${prefix}trust*
+*${prefix}dare*
+*${prefix}simi* <text>
+*${prefix}say* <text>
+
+*〈 GROUP 〉*
+*${prefix}opengc*
+*${prefix}closegc*
+*${prefix}promote* <@tag>
+*${prefix}demote* <@tag>
+*${prefix}tagall*
+*${prefix}tagall2*
+*${prefix}tagall3*
+*${prefix}tagall4*
+*${prefix}tagall5*
+*${prefix}add* <12542123926>
+*${prefix}kick* <@tag>
+*${prefix}listadmins*
+*${prefix}linkgroup*
+*${prefix}leave*
+*${prefix}welcome* <1/0>
+*${prefix}nsfw* <1/0>
+*${prefix}delete*
+*${prefix}simih* <1/0>
+*${prefix}tagme*
+*${prefix}ownergroup*
+*${prefix}antilinkgroup* <1/0>
+
+*〈 IMAGE MAKER 〉*
+*${prefix}bpink* <text>
+*${prefix}snowwrite* <text|text>
+*${prefix}3dtext* <text>
+*${prefix}firetext* <text>
+*${prefix}glitch* <text|text>
+*${prefix}shadow* <text>
+*${prefix}burnpaper* <text>
+*${prefix}coffee* <text>
+*${prefix}lovepaper* <text>
+*${prefix}woodblock* <text>
+*${prefix}qowheart* <text>
+*${prefix}mutgrass* <text>
+*${prefix}undergocean* <text>
+*${prefix}woodenboards* <text>
+*${prefix}wolfmetal* <text>
+*${prefix}metalictglow* <text>
+*${prefix}8bit* <text|text>
+*${prefix}herrypotter* <text>
+
+*〈 INFORMATION 〉*
+*${prefix}bahasa*
+*${prefix}kodenegara*
+*${prefix}kbbi* <question>
+*${prefix}fakta*
+*${prefix}infocuaca* <area>
+*${prefix}infogempa*
+*${prefix}covidcountry* <country>
+
+*〈 ISLAM 〉*
+*${prefix}quran*
+*${prefix}jsholat* <area>
+
+*〈 KERANG AJAIB 〉*
+*${prefix}apakah* <optional>
+*${prefix}bisakah* <optional>
+*${prefix}kapankah* <optional>
+*${prefix}watak*
+*${prefix}hobby*
+*${prefix}gantengcek*
+*${prefix}cantikcek*
+
+*〈 LIMIT 〉*
+*${prefix}limit*
+*${prefix}bal*
+*${prefix}buylimit* <count>
+
+*〈 LOGO MAKER 〉*
+*${prefix}ninjalogo* <text|text>
+*${prefix}logowolf* <text|text>
+*${prefix}logowolf2* <text|text>
+*${prefix}phlogo* <text|text>
+*${prefix}neonlogo* <text>
+*${prefix}neonlogo2* <text>
+*${prefix}lionlogo* <text|text>
+*${prefix}jokerlogo* <text>
+*${prefix}pubglogo* <text|text>
+
+*〈 MEME 〉*
+*${prefix}meme*
+*${prefix}memeindo*
+
+*〈 MUSIC 〉*
+*${prefix}play* <music_name>
+*${prefix}joox* <music_name>
+*${prefix}lirik* <music_name>
+*${prefix}chord* <music_name>
+
+*〈 OTHER 〉*
+*${prefix}send*
+*${prefix}wame*
+*${prefix}virtex*
+*${prefix}qrcode* <text>
+*${prefix}timer*
+*${prefix}fml*
+*${prefix}fml2*
+
+*〈 OWNER 〉*
+*${prefix}setprefix* <text/optional>
+*${prefix}block* <@tag>
+*${prefix}unblock* <@tag>
+*${prefix}bc* <text>
+*${prefix}clone* <@tag>
+*${prefix}clearall
+
+*〈 SEARCH 〉*
+*${prefix}wait
+*${prefix}ytsearch <query>
+*${prefix}trendtwit
+
+*〈 TEXT TO SPEACH 〉*
+*${prefix}tts* <language code|text>
+
+*〈 STALK 〉*
+*${prefix}tiktokstalk* <username>
+*${prefix}igstalk* <username>
+
+*〈 STAY ON SCREEN 〉*
+*${prefix}afk* <reason>
+
+*〈 TRUST OR DARE 〉*
+*${prefix}trust*
+*${prefix}dare*
+
+*〈 WEEBOO 〉*
+*${prefix}neonime*
+*${prefix}pokemon*
+*${prefix}loli*
+*${prefix}waifu*
+*${prefix}randomanime*
+*${prefix}husbu*
+*${prefix}husbu2*
+*${prefix}wait*
+*${prefix}nekonime*
+  
+*〈 LEVEL 〉*
+*${prefix}level*
+*${prefix}leveling* <1/0>
+*${prefix}mining*
+*${prefix}event* <1/0>
+
+*Developer © ${botName}`
+}
+exports.help = help
